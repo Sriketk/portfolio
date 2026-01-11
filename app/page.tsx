0b45106e01634ee49a83b0fca18d1d31388ed48a
Sriket Komali <sriketk5@gmail.com>
2026-01-10 22:16:58 -0500

    typo on marty supreme

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
               className="underline"
             >
               Marty Supreme
-            </Link>{" "}s
+            </Link>{" "}
             and{" "}
             <Link
               href="https://letterboxd.com/film/avatar-fire-and-ash/"
